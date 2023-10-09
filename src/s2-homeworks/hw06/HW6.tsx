ca657bcae0e32f5cafc454b2b40057b632eaba23
eleonora <nebrudko18@gmail.com>
2023-10-09 14:45:28 +0300

    fix hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -11,11 +11,11 @@ const HW6 = () => {
   const [value, setValue] = useState<string>("")
 
   const save = () => {
-    return saveState<string>('hw6-save', "")
+    return saveState<string>("hw6-editable-span", value)
   }
 
   const restore = () => {
-    let restor = restoreState<string>('hw6-save', value)
+    let restor = restoreState<string>("hw6-editable-span", value)
     setValue(restor)
   }
 
