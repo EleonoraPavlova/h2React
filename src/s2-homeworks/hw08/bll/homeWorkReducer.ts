52b0fc863ed489e57e17c9e371f4503e5af05e6e
eleonora <nebrudko18@gmail.com>
2023-10-10 12:44:54 +0300

    added styles hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,11 +4,11 @@ type ActionType =
   | { type: 'sort'; payload: 'up' | 'down' }
   | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
   switch (action.type) {
     case 'sort': { // by name
-
-      return state // need to fix
+      let copyState = state
+      return copyState.sort() // need to fix
     }
     case 'check': {
 
--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -10,15 +10,15 @@ import s2 from '../../s1-main/App.module.css'
 * */
 
 const HW9 = () => {
-    return (
-        <div id={'hw9'}>
-            <div className={s2.hwTitle}>Homework #9</div>
-
-            <div className={s2.hw}>
-                <Clock />
-            </div>
-        </div>
-    )
+  return (
+    <div id={'hw9'} className={s2.hw1}>
+      <div className={s2.hwTitle}>Case #9</div>
+      <hr className={s2.hr} />
+      <div className={s2.hw}>
+        <Clock />
+      </div>
+    </div>
+  )
 }
 
 export default HW9
