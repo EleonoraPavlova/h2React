d27aa9ed220044b8598b8e2ed9737548eb7374e4
eleonora <nebrudko18@gmail.com>
2023-10-10 14:12:47 +0300

    fix hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -11,13 +11,13 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
       if (action.payload === "up") {
         return copyState.sort((a, b) => a.name.localeCompare(b.name))
       }
-      // if (action.payload === "down") {
-
-      // }
+      if (action.payload === "down") {
+        return copyState.sort((a, b) => b.name.localeCompare(a.name))
+      }
       return state
     }
     case 'check': {
-      return state.filter(u => u.age >= 18)
+      return state.filter(u => u.age >= action.payload)
     }
     default:
       return state
